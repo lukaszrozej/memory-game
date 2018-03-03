fc29f9041e1147b133e268318be0b80029ebf911
Łukasz Rożej <lukasz.rozej@gmail.com>
2018-03-03 10:13:29 +0100

    feat: If card is matched don't do anything

--- app.js
+++ app.js
@@ -14,6 +14,10 @@ function markAsMatched(card) {
   card.classList.add('matched');
 }
 
+function cardIsMatched(card) {
+  return card.classList.contains('matched');
+}
+
 function cardsMatch(card1, card2) {
   return card1.dataset.card === card2.dataset.card;
 }
@@ -23,6 +27,7 @@ const table = document.querySelector('.table');
 table.addEventListener('click', function(event) {
   if (event.target.nodeName === 'LI') {
     const currentCard = event.target;
+    if (cardIsMatched(currentCard)) return;
     showCard(currentCard);
     if (previousCard) {
       if (cardsMatch(currentCard, previousCard)) {
