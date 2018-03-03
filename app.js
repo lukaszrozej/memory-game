43d4a8eeae0dfef8bcdd170e5477ba2b632765fd
Łukasz Rożej <lukasz.rozej@gmail.com>
2018-03-03 10:07:46 +0100

    feat: Add main game logic

--- app.js
+++ app.js
@@ -1,14 +1,40 @@
+let previousCard = undefined;
 
 function showCard(card) {
   card.classList.add('show');
+}
+
+function hideCard(card) {
+  setTimeout(function() {
+    card.classList.remove('show');
+  }, 1000);
+}
+
+function markAsMatched(card) {
+  card.classList.add('matched');
+}
+
+function cardsMatch(card1, card2) {
+  return card1.dataset.card === card2.dataset.card;
 }
 
 const table = document.querySelector('.table');
 
 table.addEventListener('click', function(event) {
   if (event.target.nodeName === 'LI') {
-    const card = event.target;
-    showCard(card);
-    console.log('Card was clicked', card);
+    const currentCard = event.target;
+    showCard(currentCard);
+    if (previousCard) {
+      if (cardsMatch(currentCard, previousCard)) {
+        markAsMatched(currentCard);
+        markAsMatched(previousCard);
+      } else {
+        hideCard(currentCard);
+        hideCard(previousCard);
+      }
+      previousCard = undefined;
+    } else {
+      previousCard = currentCard;
+    }
   }
 });
