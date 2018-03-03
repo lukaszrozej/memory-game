62b1d0682401f2c3a3c62e783482cea7cb7dd563
Łukasz Rożej <lukasz.rozej@gmail.com>
2018-03-03 09:31:35 +0100

    feat: Show cards when clicked

--- app.js
+++ app.js
@@ -1,8 +1,14 @@
+
+function showCard(card) {
+  card.classList.add('show');
+}
+
 const table = document.querySelector('.table');
 
 table.addEventListener('click', function(event) {
   if (event.target.nodeName === 'LI') {
     const card = event.target;
+    showCard(card);
     console.log('Card was clicked', card);
   }
 });
