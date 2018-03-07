5ba820e2c25d433390fbcc3694aa456062ebd583
Łukasz Rożej <lukasz.rozej@gmail.com>
2018-03-07 06:59:43 +0100

    refactor: Turn deck array into array of decks

--- js/decks.js
+++ js/decks.js
@@ -1,22 +1,25 @@
-const deck = [
-  "robot-1",
-  "robot-2",
-  "robot-design",
-  "robot-front",
-  "robot-machine",
-  "robot-monster-of-one-eye",
-  "robot-of-circular-parts",
-  "robot-of-long-arms-and-short-legs",
-  "robot-of-rounded-head",
-  "robot-of-short-spherical-shape",
-  "robot-standing-over-wheels-with-an-antenna-on-the-head",
-  "robot-tool",
-  "robot-with-a-gear",
-  "robot-with-flexible-arms-and-legs",
-  "robot-with-gears",
-  "robot-with-lifeline",
-  "robot",
-  "rounded-robot-design-with-spirals-arms",
-  "short-robot",
-  "small-robot-with-arms-and-legs"
-]
+const decks = [{
+  name: 'robots',
+  cards: [
+    "robot-1",
+    "robot-2",
+    "robot-design",
+    "robot-front",
+    "robot-machine",
+    "robot-monster-of-one-eye",
+    "robot-of-circular-parts",
+    "robot-of-long-arms-and-short-legs",
+    "robot-of-rounded-head",
+    "robot-of-short-spherical-shape",
+    "robot-standing-over-wheels-with-an-antenna-on-the-head",
+    "robot-tool",
+    "robot-with-a-gear",
+    "robot-with-flexible-arms-and-legs",
+    "robot-with-gears",
+    "robot-with-lifeline",
+    "robot",
+    "rounded-robot-design-with-spirals-arms",
+    "short-robot",
+    "small-robot-with-arms-and-legs"
+  ]
+}]
