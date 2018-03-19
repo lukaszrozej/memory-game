bafe4d7d2e5be34cbb50ac72dc7fb2ec37be674a
Łukasz Rożej <lukasz.rozej@gmail.com>
2018-03-19 14:08:00 +0100

    chore: Rename src to sprites

--- gulpfile.js
+++ gulpfile.js
@@ -5,11 +5,11 @@ var fs = require('fs');
 
 gulp.task('default', function() {
 
-  const d = fs.readdirSync('src/decks')
+  const d = fs.readdirSync('sprites/decks')
     .map(function(deck) {
       return {
         name: deck,
-        cards: fs.readdirSync(`src/decks/${deck}`)
+        cards: fs.readdirSync(`sprites/decks/${deck}`)
           .map(card => card.replace('.svg', ''))
       }
     });
@@ -17,7 +17,7 @@ gulp.task('default', function() {
   fs.writeFileSync('js/decks.js', text);
 
   return gulp
-    .src('src/**/*.svg')
+    .src('sprites/**/*.svg')
     .pipe(svgmin())
     .pipe(svgstore())
     .pipe(gulp.dest('svg'));
