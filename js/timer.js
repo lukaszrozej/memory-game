c0744304698a23e4d3e4f8b300061f77c3dfc929
Łukasz Rożej <lukasz.rozej@gmail.com>
2018-03-19 16:32:22 +0100

    refactor: Move initializeTimer to separate file

---
+++ js/timer.js
@@ -0,0 +1,55 @@
+function initializeTimer(element) {
+  let startTime = 0;
+  let elapsedTime = 0;
+  let intervalId;
+  let running = false;
+  let paused = false;
+  let pauseStart;
+  let totalPausedTime = 0;
+
+  function display() {
+    if (paused) return;
+    element.textContent = Math.round((Date.now() - startTime - totalPausedTime) / 1000).toString();
+  }
+
+  return {
+    isRunning() {
+      return running;
+    },
+
+    start() {
+      startTime = Date.now();
+      intervalId = setInterval(display, 1000);
+      running = true;
+    },
+
+    pause() {
+      paused = true;
+      pauseStart = Date.now();
+    },
+
+    resume() {
+      totalPausedTime += Date.now() - pauseStart;
+      paused = false;
+    },
+
+    stop() {
+      clearInterval(intervalId);
+      running = false;
+      elapsedTime = Math.round((Date.now() - startTime - totalPausedTime) / 1000);
+      element.textContent = elapsedTime.toString();
+    },
+
+    reset() {
+      clearInterval(intervalId);
+      running = false;
+      paused = false;
+      totalPausedTime = 0;
+      element.textContent = '0';
+    },
+
+    value() {
+      return elapsedTime;
+    }
+  };
+}
