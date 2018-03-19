7475f63517d2bb90470f54246b27e46a0d46625a
Łukasz Rożej <lukasz.rozej@gmail.com>
2018-03-19 15:11:46 +0100

    refactor: Put everything into startApp

--- js/app.js
+++ js/app.js
@@ -1,142 +1,143 @@
-//-----------------------------------------------------------------------------
-// DOM elements
-const $decks = document.querySelector('.decks');
+function startApp(decks) {
 
-const $movesCounter = document.querySelector('.moves-counter');
-const $stars = document.querySelectorAll('.star');
-const $table = document.querySelector('.table');
-// Buttons:
-const $restartBtn = document.querySelector('.restart-btn')
-const $helpBtn = document.querySelector('.help-btn')
+  //-----------------------------------------------------------------------------
+  // DOM elements
+  const $decks = document.querySelector('.decks');
 
-let game;
+  const $movesCounter = document.querySelector('.moves-counter');
+  const $stars = document.querySelectorAll('.star');
+  const $table = document.querySelector('.table');
+  // Buttons:
+  const $restartBtn = document.querySelector('.restart-btn')
+  const $helpBtn = document.querySelector('.help-btn')
 
-//-----------------------------------------------------------------------------
-// Helper functions
+  let game;
 
-// Taken from https://stackoverflow.com/a/39914235/2897430
-function sleep(ms) {
-  return new Promise(resolve => setTimeout(resolve, ms));
-}
+  //-----------------------------------------------------------------------------
+  // Helper functions
 
-function starsFromMoves(numberOfMoves) {
-  const tresholds = [13, 17, 21];
-  let i;
-  for(i = 0; i < tresholds.length; i++) {
-    if (numberOfMoves < tresholds[i]) break;
-  }
-  return 3 - i;
-}
-
-//-----------------------------------------------------------------------------
-// Timer
-
-function initializeTimer(element) {
-  let startTime = 0;
-  let elapsedTime = 0;
-  let intervalId;
-  let running = false;
-  let paused = false;
-  let pauseStart;
-  let totalPausedTime = 0;
-
-  function display() {
-    if (paused) return;
-    element.textContent = Math.round((Date.now() - startTime - totalPausedTime) / 1000).toString();
+  // Taken from https://stackoverflow.com/a/39914235/2897430
+  function sleep(ms) {
+    return new Promise(resolve => setTimeout(resolve, ms));
   }
 
-  return {
-    isRunning() {
-      return running;
-    },
-
-    start() {
-      startTime = Date.now();
-      intervalId = setInterval(display, 1000);
-      running = true;
-    },
-
-    pause() {
-      paused = true;
-      pauseStart = Date.now();
-    },
-
-    resume() {
-      totalPausedTime += Date.now() - pauseStart;
-      paused = false;
-    },
-
-    stop() {
-      clearInterval(intervalId);
-      running = false;
-      elapsedTime = Math.round((Date.now() - startTime - totalPausedTime) / 1000);
-      element.textContent = elapsedTime.toString();
-    },
-
-    reset() {
-      clearInterval(intervalId);
-      running = false;
-      paused = false;
-      totalPausedTime = 0;
-      element.textContent = '0';
-    },
-
-    value() {
-      return elapsedTime;
+  function starsFromMoves(numberOfMoves) {
+    const tresholds = [13, 17, 21];
+    let i;
+    for(i = 0; i < tresholds.length; i++) {
+      if (numberOfMoves < tresholds[i]) break;
     }
-  };
-}
+    return 3 - i;
+  }
 
-const timer = initializeTimer(document.querySelector('.time'));
+  //-----------------------------------------------------------------------------
+  // Timer
 
-//-----------------------------------------------------------------------------
-// Cards
+  function initializeTimer(element) {
+    let startTime = 0;
+    let elapsedTime = 0;
+    let intervalId;
+    let running = false;
+    let paused = false;
+    let pauseStart;
+    let totalPausedTime = 0;
 
-function getCard(event) {
-  const card = event.target.closest('.card');
-  if (!card) return undefined;
+    function display() {
+      if (paused) return;
+      element.textContent = Math.round((Date.now() - startTime - totalPausedTime) / 1000).toString();
+    }
 
-  return {    
-    show() {
-      return new Promise(function(resolve) {
-        function handleTransisionEnd(event) {
-          card.removeEventListener('transitionend', handleTransisionEnd);
-          resolve();
-        }
-        card.addEventListener('transitionend', handleTransisionEnd);
-        card.classList.add('show');
-      });
-    },
+    return {
+      isRunning() {
+        return running;
+      },
 
-    hide() {
-      card.classList.remove('show', 'not-matched', 'matched');
-    },
+      start() {
+        startTime = Date.now();
+        intervalId = setInterval(display, 1000);
+        running = true;
+      },
 
-    markAs(status) {
-      return new Promise(function(resolve) {
-        function handleAnimationEnd(event) {
-          card.removeEventListener('animationend', handleAnimationEnd);
-          resolve();
-        }
-        card.addEventListener('animationend', handleAnimationEnd);
-        card.classList.add(status);
-      });
-    },
+      pause() {
+        paused = true;
+        pauseStart = Date.now();
+      },
 
-    id() {
-      return card.dataset.card;
-    },
+      resume() {
+        totalPausedTime += Date.now() - pauseStart;
+        paused = false;
+      },
 
-    isFlipped() {
-      return card.classList.contains('show');
+      stop() {
+        clearInterval(intervalId);
+        running = false;
+        elapsedTime = Math.round((Date.now() - startTime - totalPausedTime) / 1000);
+        element.textContent = elapsedTime.toString();
+      },
+
+      reset() {
+        clearInterval(intervalId);
+        running = false;
+        paused = false;
+        totalPausedTime = 0;
+        element.textContent = '0';
+      },
+
+      value() {
+        return elapsedTime;
+      }
+    };
+  }
+
+  const timer = initializeTimer(document.querySelector('.time'));
+
+  //-----------------------------------------------------------------------------
+  // Cards
+
+  function getCard(event) {
+    const card = event.target.closest('.card');
+    if (!card) return undefined;
+
+    return {    
+      show() {
+        return new Promise(function(resolve) {
+          function handleTransisionEnd(event) {
+            card.removeEventListener('transitionend', handleTransisionEnd);
+            resolve();
+          }
+          card.addEventListener('transitionend', handleTransisionEnd);
+          card.classList.add('show');
+        });
+      },
+
+      hide() {
+        card.classList.remove('show', 'not-matched', 'matched');
+      },
+
+      markAs(status) {
+        return new Promise(function(resolve) {
+          function handleAnimationEnd(event) {
+            card.removeEventListener('animationend', handleAnimationEnd);
+            resolve();
+          }
+          card.addEventListener('animationend', handleAnimationEnd);
+          card.classList.add(status);
+        });
+      },
+
+      id() {
+        return card.dataset.card;
+      },
+
+      isFlipped() {
+        return card.classList.contains('show');
+      }
     }
   }
-}
 
-//-----------------------------------------------------------------------------
-// Modal
+  //-----------------------------------------------------------------------------
+  // Modal
 
-function initializeModal(decks) {
 
   const $modal = document.querySelector('.modal');
 
@@ -238,108 +239,109 @@ function initializeModal(decks) {
     }
   });
 
+
+  //-----------------------------------------------------------------------------
+  // Game
+
+  function newGame() {
+
+    const numberOfCards = 8;
+
+    const openCards = [];
+    let numberOfCLicks = 0;
+    let numberOfMoves = 0;
+    let numberOfMatched = 0;
+
+    // Shuffle:
+    const order = [...Array(numberOfCards).keys(), ...Array(numberOfCards).keys()];
+    for(let i = order.length-1; i > 0; i--) {
+      const j = Math.floor(Math.random() * i);
+      [order[i], order[j]] = [order[j], order[i]];
+    }
+    // Deal:
+    const deckNumber = $decks.value;
+    $table.innerHTML = order
+      .map(cardNumber => `
+        <li class="card" data-card="${cardNumber}">
+          <div class="back">
+            <svg class="icon">
+              <use xlink:href="svg/sprites.svg#question-mark"></use>
+            </svg>
+          </div>
+          <div class="front">
+            <svg class="icon">
+              <use xlink:href="svg/sprites.svg#${decks[deckNumber].cards[cardNumber]}"></use>
+            </svg>
+          </div>
+        </li>`)
+      .join('\n');
+
+
+    function updateScore() {
+      $movesCounter.textContent = numberOfMoves;
+      const numberOfStars = starsFromMoves(numberOfMoves);
+      if (numberOfStars === 3) {
+        $stars.forEach(star => star.classList.remove('lost'));
+        return;
+      }
+      $stars[numberOfStars].classList.add('lost');
+    }
+
+    timer.reset();
+    updateScore();
+
+    async function clickHandler(event) {
+      const currentCard = getCard(event);
+      if (!currentCard || currentCard.isFlipped()) return;
+
+      if (!timer.isRunning()) {
+        timer.start();
+      }
+
+      openCards.push(currentCard);
+      numberOfCLicks++;
+
+      if (numberOfCLicks % 2 === 0) {
+        numberOfMoves++;
+        updateScore();
+        const previousCard = openCards[numberOfCLicks-2];
+        if (currentCard.id() === previousCard.id()) {
+          numberOfMatched++;
+          await currentCard.show();
+          await Promise.all([
+            currentCard.markAs('matched'),
+            previousCard.markAs('matched')
+          ]);
+          if (numberOfMatched === numberOfCards) {
+            timer.stop();
+            modal.updateWinMessage(numberOfMoves);
+            modal.show('win');
+          }
+        } else {
+          await currentCard.show();
+          await Promise.all([
+            currentCard.markAs('not-matched'),
+            previousCard.markAs('not-matched')
+          ]);
+          currentCard.hide();
+          previousCard.hide();
+        }
+      } else {
+        currentCard.show();
+      }
+    };
+
+    $table.addEventListener('click', clickHandler);
+
+    return { 
+      stop() {
+        // To avoid memory leaks
+        $table.removeEventListener('click', clickHandler);
+      }
+    };
+  }
+
   return { show, updateWinMessage };
 }
 
-//-----------------------------------------------------------------------------
-// Game
-
-function newGame() {
-
-  const numberOfCards = 8;
-
-  const openCards = [];
-  let numberOfCLicks = 0;
-  let numberOfMoves = 0;
-  let numberOfMatched = 0;
-
-  // Shuffle:
-  const order = [...Array(numberOfCards).keys(), ...Array(numberOfCards).keys()];
-  for(let i = order.length-1; i > 0; i--) {
-    const j = Math.floor(Math.random() * i);
-    [order[i], order[j]] = [order[j], order[i]];
-  }
-  // Deal:
-  const deckNumber = $decks.value;
-  $table.innerHTML = order
-    .map(cardNumber => `
-      <li class="card" data-card="${cardNumber}">
-        <div class="back">
-          <svg class="icon">
-            <use xlink:href="svg/sprites.svg#question-mark"></use>
-          </svg>
-        </div>
-        <div class="front">
-          <svg class="icon">
-            <use xlink:href="svg/sprites.svg#${decks[deckNumber].cards[cardNumber]}"></use>
-          </svg>
-        </div>
-      </li>`)
-    .join('\n');
-
-
-  function updateScore() {
-    $movesCounter.textContent = numberOfMoves;
-    const numberOfStars = starsFromMoves(numberOfMoves);
-    if (numberOfStars === 3) {
-      $stars.forEach(star => star.classList.remove('lost'));
-      return;
-    }
-    $stars[numberOfStars].classList.add('lost');
-  }
-
-  timer.reset();
-  updateScore();
-
-  async function clickHandler(event) {
-    const currentCard = getCard(event);
-    if (!currentCard || currentCard.isFlipped()) return;
-
-    if (!timer.isRunning()) {
-      timer.start();
-    }
-
-    openCards.push(currentCard);
-    numberOfCLicks++;
-
-    if (numberOfCLicks % 2 === 0) {
-      numberOfMoves++;
-      updateScore();
-      const previousCard = openCards[numberOfCLicks-2];
-      if (currentCard.id() === previousCard.id()) {
-        numberOfMatched++;
-        await currentCard.show();
-        await Promise.all([
-          currentCard.markAs('matched'),
-          previousCard.markAs('matched')
-        ]);
-        if (numberOfMatched === numberOfCards) {
-          timer.stop();
-          modal.updateWinMessage(numberOfMoves);
-          modal.show('win');
-        }
-      } else {
-        await currentCard.show();
-        await Promise.all([
-          currentCard.markAs('not-matched'),
-          previousCard.markAs('not-matched')
-        ]);
-        currentCard.hide();
-        previousCard.hide();
-      }
-    } else {
-      currentCard.show();
-    }
-  };
-
-  $table.addEventListener('click', clickHandler);
-
-  return { 
-    stop() {
-      // To avoid memory leaks
-      $table.removeEventListener('click', clickHandler);
-    }
-  };
-}
-
-const modal = initializeModal(decks);
+const modal = startApp(decks);
